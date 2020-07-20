3f0d81573f4d3552ef0241e027ec1af15cc73d31
vijayarasan <58214164+vijayarasan@users.noreply.github.com>
2020-07-20 20:32:22 +0530

    How to implement Equals Search type in SearchController in WPF DataGrid(SfDataGrid)?
    How to implement Equals Search type in SearchController in WPF DataGrid(SfDataGrid)?

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Syncfusion.Data;
+using Syncfusion.UI.Xaml.Grid;
+using Syncfusion.UI.Xaml.Grid.Helpers;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SfDataGridDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ViewModel viewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            viewModel = this.DataContext as ViewModel;
+            this.sfDataGrid.SearchHelper = new SearchHelperExt(this.sfDataGrid);
+            btnFind.Click += BtnFind_Click;
+        }
+
+        private void BtnFind_Click(object sender, RoutedEventArgs e)
+        {
+            this.sfDataGrid.SearchHelper.ClearSearch();
+            this.sfDataGrid.SearchHelper.FindNext(txtSearch.Text);
+            this.sfDataGrid.SearchHelper = new SearchHelperExt(this.sfDataGrid);            
+        }
+    }
+
+    public class SearchHelperExt : SearchHelper
+    {
+        public SearchHelperExt(SfDataGrid datagrid)
+            : base(datagrid)
+        {
+           
+        }
+
+        protected override bool MatchSearchText(GridColumn column, object record)
+        {        
+
+                var cellvalue = Provider.GetFormattedValue(record, column.MappingName);
+           
+                if (!AllowCaseSensitiveSearch)
+                    return cellvalue != null && cellvalue.ToString().ToLower().Equals(SearchText.ToString().ToLower());
+                else
+                    return cellvalue != null && cellvalue.ToString().Equals(SearchText.ToString());           
+        }        
+    }
+}
+         
+   
+
